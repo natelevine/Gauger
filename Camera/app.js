513161428e4fc1cb0c101972065dc7a161d3b62b
Nathan Levine <natejlevine@gmail.com>
2016-02-13 20:15:32 -0800

    added taking and saving video support, and script for pulling out keyframes

---
+++ Camera/app.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+var ThetaSOscClient = require('osc-client-theta_s').ThetaSOscClient;
+var Ffmpeg = require('fluent-ffmpeg');
+var getFrames = require('./capture-frames.js');
+
+var domain = '192.168.1.1';
+var port = '80';
+
+var thetaClient = new ThetaSOscClient(domain, port);
+var sessionId;
+var fileName;
+
+thetaClient.startSession().then(function (res) {
+  //Grab the session id
+  sessionId = res.body.results.sessionId;
+  return thetaClient.setOptions(sessionId, {captureMode:"_video"})
+})
+.then(function (res) {
+  console.log('starting capture');
+  return thetaClient.startCapture(sessionId);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+// Stop capturing after a certain interval in ms
+var interval = 5000;
+var count = 2;
+
+setTimeout(function () {
+  console.log('stopping capture');
+  thetaClient.stopCapture(sessionId)
+  .then(function (res) {
+    console.log('stopped capture, writing file');
+    // need to get the unique video uri
+    return thetaClient.listAll({entryCount:1, sort:"newest"});
+  })
+  .then(function (res) {
+    fileName = res.body.results.entries[0].name;
+    console.log(fileName);
+    return thetaClient.getVideo(res.body.results.entries[0].uri, "full");
+  })
+  .then(function (res) {
+    fs.writeFile(fileName, res.body);
+  })
+  .then(function (err) {
+    console.log('closing the session');
+    return thetaClient.closeSession(sessionId);
+  })
+  // clip keyframes and save them
+  .then(function () {
+    return getFrames(fileName, count);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}, interval);
---
+++ Camera/capture-frames.js
@@ -0,0 +1,31 @@
+var Ffmpeg = require('fluent-ffmpeg');
+
+module.exports = function (filepath, count) {
+
+  var computeTime = function(length, count) {
+    var timemarks = [];
+    var timemark = 1;
+    if (count > length-1) {
+      console.log('cannot clip ' + count + ' frames');
+    }
+    var interval = length / count;
+    while (timemark <= length) {
+      timemarks.push(timemark);
+      timemark += interval;
+    }
+    return timemarks;
+  };
+
+  Ffmpeg.ffprobe(filepath, function(err, metadata) {
+    var duration = metadata.format.duration;
+    var proc = new Ffmpeg(filepath);
+    
+    console.log('taking key frames')
+    return proc.takeScreenshots({
+      count: count,
+      timemarks: computeTime(duration, count),
+    }, './', function (err) {
+      console.log('saved screenshots');
+    });
+  });
+};
