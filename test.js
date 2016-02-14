95f1636d4c72bea5266987a429eac59e0353cfd5
Nathan Levine <natejlevine@gmail.com>
2016-02-13 21:42:01 -0800

    added test node script for running py files

---
+++ test.js
@@ -0,0 +1,29 @@
+var PythonShell = require('python-shell');
+
+var options = {
+  mode: 'text',
+  pythonPath: '/usr/local/bin/python',
+  pythonOptions: [],
+  scriptPath: './Speech2Text',
+  args: []
+};
+//'/Users/natelevine/DevWeek16/Hackathon/Gauger/Speech2Text/R0010125.MP4'
+
+PythonShell.run('analyze_sentiment.py', options, function (err, results) {
+  if (err) throw err;
+
+  console.log('results: ', results);
+})
+
+// var pyshell = new PythonShell();
+//
+// pyshell.send('/Users/natelevine/DevWeek16/Hackathon/Gauger/Speech2Text/R0010125.MP4');
+//
+// pyshell.on('message', function(message) {
+//   console.log('message from py script: ', message);
+// });
+//
+// pyshell.end(function (err) {
+//   if (err) throw err;
+//   console.log('finished');
+// });
